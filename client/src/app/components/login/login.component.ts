2cf7e460d8c6ad2f88fe7fe1291401275fab6ba0
Darkweak <sylvain@les-tilleuls.coop>
2021-01-05 02:45:54 +0100

    feature: Imporove login

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
 import { Token } from 'src/app/storage/Token';
 import { Locale } from 'src/app/storage/Locale';
+import { ConnectionService } from 'src/app/services/connection.service';
 
 @Component({
   selector: 'app-login',
@@ -14,29 +13,15 @@ export class LoginComponent implements OnInit {
   public username: string;
   public password: string;
   public hide = true;
-  private _options = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-  };
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private connectionService: ConnectionService, private router: Router) {}
 
   ngOnInit(): void {}
 
   submit() {
-    const formData: FormData = new FormData();
-    formData.append('username', this.username);
-    formData.append('password', this.password);
-
-    const object = {};
-    formData.forEach(function (value, key) {
-      object[key] = value;
-    });
-
-    const json = JSON.stringify(object);
-
-    this.http
-      .post(`${environment.HOST}/authentication_token`, json, this._options)
-      .subscribe(({ token }: { token?: string }) => {
+    this.connectionService
+        .login({ username: this.username, password: this.password })
+        .subscribe(({ token }: { token?: string }) => {
         if (token) {
           new Token().set(token);
           const userLang = navigator.language;
--- client/src/app/services/connection.service.ts
+++ client/src/app/services/connection.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { AbstractService } from 'src/app/services/abstract';
 import { Token } from 'src/app/storage/Token';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -19,7 +20,12 @@ export class ConnectionService extends AbstractService {
     return `${environment.HOST}/${this.endpoint}`;
   }
 
-  logout() {
+  login(data): Observable<Object> {
+    this.updateHeaders({ 'Content-Type': 'application/json' })
+    return this.insert(data)
+  }
+
+  logout(): void {
     new Token().reset();
     this.router.navigateByUrl('/login');
   }

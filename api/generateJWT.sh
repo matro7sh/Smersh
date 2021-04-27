f3952d27b1d78de6b340a7c6881ef9b94acc8c45
Aurelien Chalot <aurelien.chalot@orangecyberdefense.com>
2021-04-27 09:48:41 +0200

    Remove the ls -al debug command in api/generateJWT.sh

--- api/generateJWT.sh
+++ api/generateJWT.sh
@@ -7,7 +7,6 @@ docker-compose exec php sh -c '
     apk add openssl
     mkdir -p config/jwt
     jwt_passphrase='$1'
-    ls -al config/jwt 
     echo "$jwt_passphrase" | openssl genpkey -out config/jwt/private.pem -pass stdin -aes256 -algorithm rsa -pkeyopt rsa_keygen_bits:4096
     echo "$jwt_passphrase" | openssl pkey -in config/jwt/private.pem -passin stdin -out config/jwt/public.pem -pubout
     setfacl -R -m u:www-data:rX -m u:"$(whoami)":rwX config/jwt

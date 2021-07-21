9ea98459a6355e83856b809300694476698fa74d
Darkweak <sylvain@les-tilleuls.coop>
2021-07-21 05:52:23 -0400

    Fix build client

--- client/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  API_DOMAIN: '${API_BASE_URL}',
-  TRANSPORT: '${TRANSPORT}',
-  API_ENDPOINT: '/api',
-  MAPS_KEY: '',
-  version: '1.0.0',
-};
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,8 +1,9 @@
 export const environment = {
-  production: false,
+  production: true,
   API_DOMAIN: 'api.domain.com',
   TRANSPORT: 'http://',
   API_ENDPOINT: '/api',
   MAPS_KEY: '',
   version: '1.0.0',
+  environment: 'prod',
 };

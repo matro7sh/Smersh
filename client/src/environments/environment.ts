0a3229b5e27d413aae200774091ce958882979e4
Houziaux Mike <stahlilistenvoid@gmail.com>
2021-08-05 17:41:30 +0200

    Force HTTPS everywhere (#130)
    Co-authored-by: jenaye - jqck <jenaye@protonmail.com>

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  API_DOMAIN: 'api.domain.com',
+  API_DOMAIN: 'api.smersh.lan',
   TRANSPORT: 'http://',
   API_ENDPOINT: '/api',
   MAPS_KEY: '',
-  version: '1.1.1',
+  version: '1.1.2',
   environment: 'prod',
 };
